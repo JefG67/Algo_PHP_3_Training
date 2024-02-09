36034526b12685d5488d99581f77795b175e28a6
JF.G <156801075+JefG67@users.noreply.github.com>
2024-02-09 16:35:48 +0100

    Add files via upload

---
+++ Algo_3.php
@@ -0,0 +1,9 @@
+<?php
+
+
+$c =8;
+for($a=0;$a <= 10; $a++){
+    $Result = $c*$a."<br>";
+    echo $Result    ;
+
+}
---
+++ Algo_4.php
@@ -0,0 +1,10 @@
+<?php
+
+$a=array(range(1, 83));
+
+
+foreach ($a as $valeur){
+   $valeur = 
+
+//    elseif ($a)
+}
